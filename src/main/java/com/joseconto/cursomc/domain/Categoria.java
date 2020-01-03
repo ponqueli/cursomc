101193bc035052d022a1c20e65f0d15ffcc4ef4b
Jose Conto <joseantonioconto@gmail.com>
2020-01-03 17:06:15 -0300

    Ajustes no endpoint /categorias/{id} excecao

--- src/main/java/com/joseconto/cursomc/domain/Categoria.java
+++ src/main/java/com/joseconto/cursomc/domain/Categoria.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 	
@@ -20,6 +22,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference //coloca isso no lugar onde vc quer q venha os objetos associados
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
